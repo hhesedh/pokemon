532d715226c1692d22828aed4509b820e3d4a3b2
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-07-11 18:58:00 -0300

    commit inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { usePokemons } from "./hooks/usePokemons";
 
 function App() {
+  const { pokemons, loading, error, deletePokemon } = usePokemons();
+
+  if (loading) return <h1>Carregando...</h1>;
+  if (error) return <h1>Error</h1>;
+  if (pokemons.length === 0) return <h1>Lista Vazia...</h1>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pokemons.map(({ name }) => (
+        <div key={name}>
+          <h1>{name}</h1>
+          <button onClick={() => deletePokemon(name)}>apagar</button>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/hooks/usePokemons.ts
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import { pokemonUrl } from "../constants/endpoint";
+import { Request } from "../request/request";
+
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
+const { isRequestError } = Request;
+const api = new Request();
+
+interface IPokemon {
+  name: string;
+  url: string;
+}
+interface IPokemonResponse {
+  count: number;
+  previous?: string;
+  next?: string;
+  results: IPokemon[];
+}
+
+export function usePokemons() {
+  const [pokemons, setPokemons] = useState<IPokemon[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function getPokemons() {
+      try {
+        setLoading(true);
+        const response = await api.get<IPokemonResponse>(pokemonUrl);
+        if (response && response.data) {
+          const { data } = response;
+          setPokemons(data.results);
+        }
+        setError(false);
+      } catch (err) {
+        if (isRequestError(err)) {
+          console.log("é um erro de request:");
+        }
+        setError(true);
+        console.log(err);
+      }
+
+      setLoading(false);
+    }
+
+    getPokemons();
+  }, []);
+
+  async function deletePokemon(name: string) {
+    try {
+      /* aqui pode ter estado de erro ou loading */
+      const swalResponse = await MySwal.fire({
+        icon: "warning",
+        title: `<p>Tem certeza que deseja deletar o pokemon ${name}?</p>`,
+        footer: "Pokemon 2021",
+        showDenyButton: true,
+        confirmButtonText: `Ok`,
+        denyButtonText: `Cancelar`,
+      });
+      if (swalResponse.isConfirmed) {
+        const pokemonsFiltered = pokemons.filter(
+          (pokemon) => pokemon.name !== name
+        );
+        setPokemons(pokemonsFiltered);
+      }
+      await MySwal.fire({
+        icon: "success",
+        title: `<p>O pokemon ${name} foi deletado com sucesso?</p>`,
+        footer: "Pokemon 2021",
+        confirmButtonText: `Ok`,
+      });
+    } catch (err) {
+      MySwal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+    }
+  }
+
+  return { pokemons, loading, error, deletePokemon };
+}
