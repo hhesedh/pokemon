a30756dd2ed98bd4f9b26bc26e02b0e693abc83c
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-07-11 22:00:39 -0300

    adição de lógica para buscar url de image

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import "./App.css";
+import { getImageById } from "./constants/endpoint";
 import { usePokemons } from "./hooks/usePokemons";
 
+function handleUrlImage(url: string) {
+  const id = url.split("/").reverse()[1];
+  const imageUrl = getImageById(id);
+  return imageUrl;
+}
 function App() {
   const { pokemons, loading, error, deletePokemon } = usePokemons();
 
@@ -9,10 +15,11 @@ function App() {
   if (pokemons.length === 0) return <h1>Lista Vazia...</h1>;
   return (
     <div className="App">
-      {pokemons.map(({ name }) => (
+      {pokemons.map(({ name, url }) => (
         <div key={name}>
           <h1>{name}</h1>
           <button onClick={() => deletePokemon(name)}>apagar</button>
+          <img src={handleUrlImage(url)} alt="name" />
         </div>
       ))}
     </div>
--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,2 +1,6 @@
+const imageUrl =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
 export const apiBaseUrl = "https://pokeapi.co/api/v2/";
 export const pokemonUrl = `${apiBaseUrl}pokemon`;
+export const getImageById = (id: string) => `${imageUrl}${id}.png`;
